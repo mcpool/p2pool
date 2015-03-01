137f497b3c10d91ee4a43ff0f6ebf1580887e2f1
mcpool <tkalantzis@gr-linux.net>
2015-03-02 00:36:52 +0200

    Changed block explorer url

--- p2pool/bitcoin/networks/monacoin.py
+++ p2pool/bitcoin/networks/monacoin.py
@@ -20,9 +20,9 @@ POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoW
 BLOCK_PERIOD = 90 # s
 SYMBOL = 'MONA'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'monacoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Monacoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.monacoin'), 'monacoin.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'http://monacoin.org/crawler/block_crawler.php?block_hash='
-ADDRESS_EXPLORER_URL_PREFIX = 'http://monacoin.org/crawler/block_crawler.php?address=' #dummy for now, not supported by explorer
-TX_EXPLORER_URL_PREFIX = 'http://monacoin.org/crawler/block_crawler.php?transaction='
+BLOCK_EXPLORER_URL_PREFIX = 'http://mona.explorer.p2pool-stats.info/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'http://mona.explorer.p2pool-stats.info/address/' #dummy for now, not supported by explorer
+TX_EXPLORER_URL_PREFIX = 'http://mona.explorer.p2pool-stats.info/tx/'
 SANE_TARGET_RANGE = (2**256//1000000000 - 1, 2**256//1000 - 1)
 DUMB_SCRYPT_DIFF = 2**16
 DUST_THRESHOLD = 0.03e8
